7fb82e04cad281d8f8bddea273a18adda11a758d
yuklyn <642150216@qq.com>
2018-04-16 22:05:19 +0800

    配置APPKEY

--- android/app/src/main/java/com/yuklyn/flutterapp/MainActivity.java
+++ android/app/src/main/java/com/yuklyn/flutterapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.yuklyn.flutterapp;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.aliyun.alink.AlinkSDK;
+import com.aliyun.alink.business.alink.ALinkEnv;
+import com.aliyun.alink.business.login.IAlinkLoginAdaptor;
+import com.aliyun.alink.business.login.IAlinkLoginCallback;
+import com.aliyun.alink.linksdk.tools.ALog;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        Toast.makeText(this, "测试", Toast.LENGTH_SHORT).show();
+    }
 }
